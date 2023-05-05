d983a0c1a21633feca4294eb4478152321db4088
MaxPC <jhosh1212@gmail.com>
2023-05-05 22:43:53 +0700

    initial commit

---
+++ src/main/java/netology_jdbc/service/Service.java
@@ -0,0 +1,19 @@
+package netology_jdbc.service;
+
+
+import netology_jdbc.repository.Repository;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+public class Service {
+    private final Repository repository;
+
+    public Service(Repository repository) {
+        this.repository = repository;
+    }
+
+    public List<String> getProducts(String name) {
+        return repository.getProductName(name);
+    }
+}
